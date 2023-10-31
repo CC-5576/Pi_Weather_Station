41d2f7234490dfdd45e7b16456da2002b2a2ec5a
CC-5576 <ollie@tjski.net>
2023-10-31 11:52:11 +0000

    v0.5 updated sensor inputs

--- WeatherStation.py
+++ WeatherStation.py
@@ -1,3 +1,4 @@
+from keyboard import is_pressed
 from collections import namedtuple
 from time import sleep
 import numpy as np
@@ -8,9 +9,10 @@ from file_of_greatness import system_check, days_of_the_week
 sense = system_check()
 
 ws = SST.word_scrolling
+
 sleep(1)
 
-ws("power on... testing 1 2 3")
+#ws("power on... testing 1 2 3")
 
 class CurrentTime:
     def __init__(self, time, date, day):
@@ -21,7 +23,7 @@ class CurrentTime:
 
     def _validate(self):
         if not isinstance(self.time, str) or not isinstance(self.day, str) or not isinstance(self.date, str):
-            print("Invaclid time object")
+            print("Invalid time object") #i.e. GIVE ME ALL THE DATA
             self.time = "00:00"
             self.day = "Monday"
             self.date = "1970/01/01"
@@ -33,21 +35,57 @@ def time_values():
     day_current = days_of_the_week[day_current]
     value = CurrentTime(time_current, date_current, day_current)
     return(value)
-temp = sense.get_temperature()
+
+
+class CurrentReadings:
+    """returns the current temp, pressure, humidity readings in an object"""
+    def __init__(self, current_temp, current_pressure, current_humid):
+        self.temperature = current_temp
+        self.pressure = current_pressure
+        self.humidity = current_humid
+        self.validate()
+    
+    def _validate(self):
+        if not isinstance(self.temperature, float) or not isinstance(self.pressure, float) or not isinstance(self.humidity, float):
+            print("Invalid sensor data") #i.e. GIVE ME ALL THE DATA (this is just a presense check, suggest change in future 31/10/23 11:40)
+            self.temperature = 18
+            self.pressure = 1013
+            self.humidity = 45
+
+
+def readings():
+    temperature = sense.get_temperature()
+    pressure = sense.get_pressure()
+    humidity = sense.get_humidity()
+    value = CurrentReadings(temperature, pressure, humidity)
+    return(value)
 
 
 while True:
     event = sense.stick.get_events()
+    # if is_pressed("C"):
+    #     break
     try: # because this is not always pressed it can and does error when no data input.
-        #print(event)
-        #print(event[0].direction) # the variable event is a array what holds objects that can be called be calling... 
+
+        print(event[0].direction) # the variable event is a array what holds objects that can be called be calling... 
         if event[0].direction == "up": # ..a point in the array and then the value wanted from the ditionary
             time_value = time_values()
-            ws(time_value[0].day)
+            ws(time_value.day)
             sleep(1)
             ws(time_value.date)
             sleep(1)
             ws(time_value.time)
+        
+        if event[0].direction == "down":
+            sensor_values = readings()
+            ws("temperature" + str(sensor_values.temperature))
+            sleep(1)
+            ws("pressure" + str(sensor_values.pressure))
+            sleep(1)
+            ws("humidity" + str(sensor_values.humidity))
+    except KeyboardInterrupt as e:
+        print(e)
+        break
     except Exception as e: # outputs the error message when an error state happens
         print(e)
     sleep(1)

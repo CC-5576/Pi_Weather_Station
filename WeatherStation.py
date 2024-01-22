bdab55ecb89a7445548b78196c9b4f67d26b9559
git <ollie@tjski.net>
2024-01-22 00:17:16 +0000

    dewpoint and heatindex added

--- WeatherStation.py
+++ WeatherStation.py
@@ -1,10 +1,10 @@
 '''Ollie Criddle Pi Weather Station Project Main Page'''
 #imports
 from time import sleep
-from screen_scrolling_testing import word_scrolling as word_scrolling, test_setup as ts # importing my screen scrolling code
+from screen_scrolling_testing import word_scrolling, test_setup as ts # importing my screen scrolling code
 import datetime as DT # importing the datetime module
 from seismographV2 import seismograph
-
+import math
 
 from file_of_greatness import system_check, days_of_the_week, file_iteration_count, logger, CPU_temp
 file_iteration_count = file_iteration_count()
@@ -63,6 +63,10 @@ class CurrentReadings:
         self.temperature = current_temp
         self.pressure = current_pressure
         self.humidity = current_humid
+        self.heatindex = ((-42.379 + 2.04901523*(self.temperature*(9/5)+32) + 10.14333127*self.humidity - .22475541*(self.temperature*(9/5)+32)*self.humidity - .00683783*(self.temperature*(9/5)+32)*(self.temperature*(9/5)+32) - .05481717*self.humidity*self.humidity + .00122874*(self.temperature*(9/5)+32)*(self.temperature*(9/5)+32)*self.humidity + .00085282*(self.temperature*(9/5)+32)*self.humidity*self.humidity - .00000199*(self.temperature*(9/5)+32)*(self.temperature*(9/5)+32)*self.humidity*self.humidity)-32)*(5/9)
+        #https://www.toppr.com/guides/physics-formulas/heat-index-formula/
+        self.dewpoint = (237.3*((math.log(self.humidity/100)+((17.27*self.temperature)/(237.3+self.temperature)))/17.27))/(1-((math.log(self.humidity/100)+((17.27*self.temperature)/(237.3+self.temperature)))/17.27))
+        #https://ag.arizona.edu/azmet/dewpoint.html
         self._validate()
     
     def _validate(self):
@@ -72,6 +76,7 @@ class CurrentReadings:
             self.pressure = 1013
             self.humidity = 45
             self.error = "Invalid sensor data"
+
 
 def readings_TPH():
     '''Collects the Temperature, Pressure and Humidity and uses the above class to generate and return an object containing them'''
@@ -85,7 +90,7 @@ def log(stick_data):
     '''logs all current sensor readings as gathered by reading_TMP'''
     sensor_data = readings_TPH()
     #print("I run")
-    logger.warning(f"file_iteration: {file_iteration_count} \tSensor_data(t,p,h): {sensor_data.temperature}, {sensor_data.pressure}, {sensor_data.humidity} \tloop_iteration: {iteration_count} \tstick_event: {stick_data}")
+    logger.warning(f"file_iteration: {file_iteration_count} \tSensor_data(t,p,h,HI,DP): {sensor_data.temperature}, {sensor_data.pressure}, {sensor_data.humidity}, {sensor_data.headindex}, {sensor_data.dewpoint} \tloop_iteration: {iteration_count} \tstick_event: {stick_data}")
     print((f"file_iteration: {file_iteration_count} \tSensor_data(t,p,h): {sensor_data.temperature}, {sensor_data.pressure}, {sensor_data.humidity} \tloop_iteration: {iteration_count} \tstick_event: {stick_data}"))
 
         
@@ -102,13 +107,13 @@ def time_output():
 def sensor_output():
     '''Outputs the temperature, pressure, humidity values from the object time_values to the word scrolling module'''
     sensor_values = readings_TPH()
-    word_scrolling("temperature " + str(int(sensor_values.temperature)))
+    word_scrolling("temperature " + str(int(sensor_values.temperature)), 15)
     sleep(1)
-    word_scrolling("pressure " + str(int(sensor_values.pressure)))
+    word_scrolling("pressure " + str(int(sensor_values.pressure)), 15)
     sleep(1)
-    word_scrolling("humidity " + str(int(sensor_values.humidity)) + "%")
+    word_scrolling("humidity " + str(int(sensor_values.humidity)) + "%", 15)
 
-def main(iteration_count):
+def main(iteration_count = 0):
     while True:
             # get data, apppend, etc
             event = sense.stick.get_events()
@@ -116,20 +121,32 @@ def main(iteration_count):
 
             try: # because this is not always pressed it can and does error when no data input.
 
+                if readings_TPH().temperature > 20:
+                    word_scrolling("WARNING, HIGH TEMPERATURE")
+                
+                if readings_TPH().temperature < 5:
+                    word_scrolling("WARNING, LOW TEMPERATURE")
+                
                 #print(event[0].direction) # the variable event is a array what holds objects that can be called be calling... 
                 if event[0].direction == "up": # ..a point in the array and then the value wanted from the ditionary
                     time_output()
                 
                 if event[0].direction == "down":
                     sensor_output()
+
+                if event[0].direction == "left":
+                    values = readings_TPH()
+                    word_scrolling("Dewpoint:" + str(values.dewpoint), 15)
+                    word_scrolling("Heat Index:" + str(values.heatindex), 10)
                     
+
             except Exception as e: # outputs the error message when an error state happens
                 event = e
                 #print(e)
 
             
-            log(event)
-            movment_old = movment
+            if iteration_count%10 == 0:
+                log(event)
             iteration_count += 1
             #print(iteration_count)
             sleep(1)
